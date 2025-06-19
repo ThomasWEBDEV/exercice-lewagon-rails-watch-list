89e89d7e5f9ffcc814aa0a1bb73180b690d32f39
Feret Thomas <thomas.feret@hotmail.fr>
2025-06-19 02:52:31 +0200

    Configure SolidQueue to use the 'queue' database in production and ensure application.rb is correct

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.solid_queue.connects_to = { database: { writing: :queue, reading: :queue } }
   # Configure SolidQueue to use the primary database
 
   # Ignore bad email addresses and do not raise email delivery errors.
